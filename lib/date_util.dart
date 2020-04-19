13a7690dd708916cf97359f13603606404f6583c
Lalo Martins <lalo.martins@gmail.com>
2020-04-19 14:31:54 +0300

    ‘configurable’ day start offset (except not actually configurable yet)

--- lib/date_util.dart
+++ lib/date_util.dart
@@ -3,8 +3,9 @@ import 'config.dart';
 int weekOffset([DateTime when]) =>
     ((when ?? DateTime.now()).weekday - config.weekStartsOn) % 7;
 
-DateTime startOfWeek({int weeksAgo = 0}) {
+DateTime startOfWeek({int weeksAgo = 0, bool midnight = false}) {
   final now = DateTime.now();
-  return DateTime(now.year, now.month, now.day).subtract(
+  final sow = DateTime(now.year, now.month, now.day).subtract(
       Duration(days: (now.weekday - config.weekStartsOn) % 7 + weeksAgo * 7));
+  return midnight ? sow : sow.add(Duration(minutes: config.dayOffsetMinutes));
 }
