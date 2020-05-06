1cae05250936578da62dee21f4d2fe11c0133c02
Lalo Martins <lalo.martins@gmail.com>
2020-05-06 19:25:49 +0300

    personalize the dark  theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -8,4 +8,6 @@ final wgLightTheme = ThemeData(
 final wgDarkTheme = ThemeData(
   brightness: Brightness.dark,
   primarySwatch: Colors.green,
+  primaryColor: Color.lerp(wgLightTheme.primaryColor, Colors.black, 0.3),
+  primaryColorDark: Color.lerp(wgLightTheme.primaryColor, Colors.black, 0.5),
 );
